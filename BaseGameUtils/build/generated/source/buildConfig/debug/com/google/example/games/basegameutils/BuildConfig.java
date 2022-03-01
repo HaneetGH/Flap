5f1ba5efd423d41cbae045f834a7a26c3b051a11
Haneet Singh Chhabra <haneet555@gmail.com>
2022-03-01 23:19:28 +0530

    Initial commit

---
+++ BaseGameUtils/build/generated/source/buildConfig/debug/com/google/example/games/basegameutils/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.example.games.basegameutils;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.google.example.games.basegameutils";
+  public static final String BUILD_TYPE = "debug";
+}
